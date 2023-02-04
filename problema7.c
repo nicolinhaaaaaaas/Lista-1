3f0d8efb37122b477740f8f059ba0be8befae7d9
Nicolas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-02-04 08:32:45 -0300

    Create problema7.c

---
+++ problema7.c
@@ -0,0 +1,43 @@
+/* Uma aplicação interessante de computadores é desenhar gráficos e gráficos de barras. Escreva um programa que leia cinco números (cada
+um entre 1 e 30). Para cada número lido, seu programa deve imprimir uma
+linha contendo aquele número de asteriscos adjacentes. Por exemplo, se seu
+programa lê o número sete, ele deve imprimir ∗ ∗ ∗ ∗ ∗ ∗ ∗. */
+
+#include <stdio.h>
+
+void funcao(int principal){
+int numero[5],i,n,m;
+
+	   printf("Insira 5 números entre 1 e 30\n");
+	   
+/*um laço de repetição para ler os números inseridos*/
+	for(i = 0; i <= 4; i++){
+    	printf("Número %iº: ",i+1);
+		scanf("%i",&numero[i]);
+		
+/*caso o número não esteja entre 1 e 30 terá que inserir o número novamente*/
+		while(numero[i] < 1 || numero[i] > 30){
+			printf("Insira novamente o número %iº: ",i+1);
+			scanf("%i",&numero[i]);	
+		}
+    }	
+/*Imprimir os asteriscos usando três laços de repetição*/
+	for (i = 0; i <= 4; i++){
+		for (n = 1; n <= 30; n++){
+			if (n == numero[i]){
+				for(m = 1; m <= n; m++){
+					printf("* ");   	
+				}
+				printf("\n\n");	
+			}	
+		}
+	}
+} 
+int main(){
+	
+	int Principal;
+	
+/*Criação da função*/
+	funcao(Principal);
+	return 0;
+}
